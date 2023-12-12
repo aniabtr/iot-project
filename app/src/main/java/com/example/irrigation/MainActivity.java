93e4add5a69dda803841ccf97a696371a812a681
Daniel Braun <daniel.braun@tuta.io>
2023-12-12 23:34:28 +0100

    interface updated

--- app/src/main/java/com/example/irrigation/MainActivity.java
+++ app/src/main/java/com/example/irrigation/MainActivity.java
@@ -12,15 +12,15 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     String [] crops = {"Potatoes", "Carrots", "Onions", "Peas"};
-    String [] waterFlowRate = {"6", "8", "10"};
+    //String [] waterFlowRate = {"6", "8", "10"};
     String [] coverageAreaTypes = {"Rectangle", "L-Shape", "Maximum Coverage"};
 
     AutoCompleteTextView autoCompleteTextViewCrops;
-    AutoCompleteTextView autoCompleteTextViewWaterFlowRate;
+    //AutoCompleteTextView autoCompleteTextViewWaterFlowRate;
     AutoCompleteTextView autoCompleteTextViewCoverageAreaType;
 
     ArrayAdapter<String> adapterCrops;
-    ArrayAdapter<String> adapterWaterFlow;
+    //ArrayAdapter<String> adapterWaterFlow;
     ArrayAdapter<String> adapterCoverageAreaType;
 
     @Override
@@ -41,18 +41,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        autoCompleteTextViewWaterFlowRate = findViewById(R.id.dropdown_waterFlow);
-        adapterWaterFlow = new ArrayAdapter<String>(this, R.layout.list_waterflow, waterFlowRate);
+        //autoCompleteTextViewWaterFlowRate = findViewById(R.id.dropdown_waterFlow);
+        //adapterWaterFlow = new ArrayAdapter<String>(this, R.layout.list_waterflow, waterFlowRate);
 
-        autoCompleteTextViewWaterFlowRate.setAdapter(adapterWaterFlow);
+        //autoCompleteTextViewWaterFlowRate.setAdapter(adapterWaterFlow);
 
-        autoCompleteTextViewWaterFlowRate.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*autoCompleteTextViewWaterFlowRate.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String item = adapterView.getItemAtPosition(i).toString();
                 Toast.makeText(MainActivity.this, "Item: " + item, Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
         autoCompleteTextViewCoverageAreaType = findViewById(R.id.dropdown_coverageAreaType);
         adapterCoverageAreaType = new ArrayAdapter<String>(this, R.layout.list_coverageareatype, coverageAreaTypes);
