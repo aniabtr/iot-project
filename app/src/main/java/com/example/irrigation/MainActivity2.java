edcf5d675717df27814f43aedb89aaa3c1c7ddae
dabr4003 <daniel.braun@tuta.io>
2023-12-18 14:17:53 +0100

    transition from intermediate to either first or dashboard added

--- app/src/main/java/com/example/irrigation/MainActivity2.java
+++ app/src/main/java/com/example/irrigation/MainActivity2.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private Button yes, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +34,28 @@ public class MainActivity2 extends AppCompatActivity {
                 "\nCoverage Area Value: " + coverageAreaValue +
                 "\nNumber of Irrigation Weeks: " + numberOfIrrigationWeeks;
         textViewData.setText(displayText);
+
+        yes = findViewById(R.id.yesButton);
+        cancel = findViewById(R.id.cancelBbutton);
+
+        yes.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v) {
+                // TODO save data to file
+
+                // transition to dashboard
+                intent.setClass(MainActivity2.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
+
+        cancel.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v) {
+                // transition back to MainActivity
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
